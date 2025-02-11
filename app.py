6ae4284e46ed2a8a9fe9a72c4eee863d711d4efd
harshal.pawar <harshal.pawar@choicetechlab.com>
2025-02-11 15:57:19 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+VERIFY_TOKEN = 'myToken'
+
+@app.route('/webhook', methods=['GET'])
+def verify():
+    mode = request.args.get('hub.mode')
+    token = request.args.get('hub.verify_token')
+    challenge = request.args.get('hub.challenge')
+    if mode == 'subscribe' and token == VERIFY_TOKEN:
+        return challenge, 200
+    else:
+        return 'Forbidden', 403
+
+@app.route('/webhook', methods=['POST'])
+def receive_message():
+    data = request.get_json()
+    # Process the incoming message data as needed
+    return 'EVENT_RECEIVED', 200
+
+if __name__ == '__main__':
+    app.run(port=5000)
