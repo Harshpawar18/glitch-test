76ea76248e91eb0df8b44c3c92924369d847300c
harshal.pawar <harshal.pawar@choicetechlab.com>
2025-02-11 16:37:46 +0530

    changed python version

--- start.sh
+++ start.sh
@@ -3,22 +3,25 @@
 # Exit early on errors
 set -eu
 
-# Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
+# Ensure Python 3 is used
 export PYTHONUNBUFFERED=true
-
-# Ensure the correct Python version is used
 PYTHON_VERSION=python3
 
-# Install Python 3 virtual env
+# Set up virtual environment
 VIRTUALENV=.data/venv
 
 if [ ! -d $VIRTUALENV ]; then
   $PYTHON_VERSION -m venv $VIRTUALENV
 fi
 
-# Install required packages
+# Ensure pip is installed
+$VIRTUALENV/bin/python -m ensurepip --upgrade
+
+# Upgrade pip
 $VIRTUALENV/bin/pip install --upgrade pip
+
+# Install dependencies
 $VIRTUALENV/bin/pip install -r requirements.txt
 
-# Run the Flask app with Gunicorn
+# Start the Flask app with Gunicorn on port 3000
 $VIRTUALENV/bin/gunicorn -b 0.0.0.0:3000 app:app
